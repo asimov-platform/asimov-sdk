af38f806879cb301ae006fd6a2817496008e6984
Arto Bendiken <arto@asimov.systems>
2026-09-12 07:20:36 -0700

    Import/export known types for socials.

--- rust/lib/asimov-social/src/lib.rs
+++ rust/lib/asimov-social/src/lib.rs
@@ -7,6 +7,7 @@
 
 #![no_std]
 #![forbid(unsafe_code)]
+#![allow(unused_imports)]
 #![cfg_attr(docsrs, feature(doc_cfg))]
 
 extern crate alloc;
@@ -16,3 +17,36 @@ extern crate std;
 
 mod follow_relationship;
 pub use follow_relationship::*;
+
+#[cfg(feature = "facebook")]
+pub mod facebook;
+
+#[cfg(feature = "github")]
+pub mod github;
+
+#[cfg(feature = "gravatar")]
+pub mod gravatar;
+
+#[cfg(feature = "instagram")]
+pub mod instagram;
+
+#[cfg(feature = "introco")]
+pub mod introco;
+
+#[cfg(feature = "linkedin")]
+pub mod linkedin;
+
+#[cfg(feature = "localai")]
+pub mod localai;
+
+#[cfg(feature = "luma")]
+pub mod luma;
+
+#[cfg(feature = "telegram")]
+pub mod telegram;
+
+#[cfg(feature = "whatsapp")]
+pub mod whatsapp;
+
+#[cfg(feature = "x")]
+pub mod x;
